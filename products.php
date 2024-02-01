f7cd2131f5725ad6269c6ef95e6e02aa392d931f
DelvinaShehu <delvine.shehu@gmail.com>
2024-02-01 10:00:17 +0100

    Konvertimi ne PHP files

---
+++ products.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="products.css">
+    <title>ClearVue</title>
+</head>
+
+
+<header class="header">
+    
+    <a href="logoja.png" class="logo">ClearVue</a>
+    
+    <nav class="navBar">
+
+        <a class="home" href="optika.html">Home</a>
+        <a class="home" href="AboutUs.html">About Us</a>
+        <a class="home" href="">Products</a>
+        <a class="home" href="contact.html">Contact</a>
+        <a class="home" href="LogIn forma.html">Log In</a>
+  
+    </nav>
+
+</header>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<section class="main-home">
+    <div class="main-text">
+        <h1>New <br> Collection</h1>
+        <br>
+        <h4>Shop now <b>→</b></h4>
+    </div>
+</section>
+    <div class="prodcut-text">
+        <p><b>Find your size , Find your fit</b></p>
+    </div>
+
+
+<div class="image-row">
+    <div class="image-container">
+      <img src="syzekafe.webp" alt="Default Image 1">
+      <img class="hover-image" src="syezekafemodele.webp" alt="Hover Image 1">
+      <p><b>MIMI SUNGLASSES CHOC</b></p>
+      <p><em>120.00 €</em></p>
+    </div>
+    
+    <div class="image-container">
+      <img src="cherryredsyze.webp" alt="Default Image 2">
+      <img class="hover-image" src="cherryredmodele.webp" alt="Hover Image 2">
+      <p><b>SYDNEY CHERRY </b></p>
+      <p><em>200.00 €</em></p>
+    </div>
+    
+    <div class="image-container">
+      <img src="syzezezaa.webp" alt="Default Image 3">
+      <img class="hover-image" src="syzezezamodele.webp" alt="Hover Image 3">
+      <p><b>SYDNEY TORT</b></p>
+      <p><em>250.00 €</em></p>
+    </div>
+  
+    <div class="image-container">
+        <img src="greysyze.webp" alt="Default Image 1">
+        <img class="hover-image" src="greysyzemodele.webp" alt="Hover Image 1">
+        <p><b>OCRAY CHROME</b></p>
+        <p><em>100.00 €</em></p>
+      </div>
+      
+      <div class="image-container">
+        <img src="tortemodele.webp" alt="Default Image 2">
+        <img class="hover-image" src="tortesyze.webp" alt="Hover Image 2">
+        <p><b>SYDNEY TORT</b></p>
+        <p><em>90.00 €</em></p>
+      </div>
+      
+      <div class="image-container">
+        <img src="leopardprintsyze.webp" alt="Default Image 3">
+        <img class="hover-image" src="leoprintmodele.webp" alt="Hover Image 3">
+        <p><b>IVY BONE TORT</b></p>
+        <p><em>80.00 €</em></p>
+      </div>
+      <div class="image-container">
+        <img src="printsyze.jpg" alt="Default Image 2">
+        <img class="hover-image" src="printmodele.jpg" alt="Hover Image 2">
+        <p><b>JANE IN BROWN TORTOISE</b></p>
+        <p><em>90.95 €</em></p>
+      </div>
+      
+      <div class="image-container">
+        <img src="venussyze.webp" alt="Default Image 3">
+        <img class="hover-image" src="palomamodele.webp" alt="Hover Image 3">
+        <p><b>AKI ZODIAC</b></p>
+        <p><em>96.70 €</em></p>
+      </div>
+      <div class="image-container">
+        <img src="mayasyze.jpg" alt="Default Image 2">
+        <img class="hover-image" src="mayamodele.jpg" alt="Hover Image 2">
+        <p><b>MAYA IN GLOSS BLACK</b></p>
+        <p><em>78.95 €</em></p>
+      </div>
+      
+      <div class="image-container">
+        <img src="cookiesyze.jpg" alt="Default Image 3">
+        <img class="hover-image" src="cookiemodele.jpg" alt="Hover Image 3">
+        <p><b>COOKIE IN GLOSS BLACK</b></p>
+        <p><em>69,95 €</em></p>
+      </div>
+      <div class="image-container">
+        <img src="monstersyze.jpg" alt="Default Image 2">
+        <img class="hover-image" src="monstermodele.jpg" alt="Hover Image 2">
+        <p><b>GENTLE MONSTER ROCOCO 01</b></p>
+        <p><em>244.88 €</em></p>
+      </div>
+      
+      <div class="image-container">
+        <img src="dior2syze.jpg" alt="Default Image 3">
+        <img class="hover-image" src="dior2modele.jpg" alt="Hover Image 3">
+        <p><b>DIOR</b></p>
+        <p><em>450.00 €</em></p>
+      </div>
+
+      <div class="image-container">
+        <img src="givenchysyze.avif" alt="Default Image 2">
+        <img class="hover-image" src="givenchymodele.avif" alt="Hover Image 2">
+        <p><b>Givenchy Cut cat-eye nylon sunglasses</b></p>
+        <p><em>341.99 €</em></p>
+      </div>
+      
+      <div class="image-container">
+        <img src="squaresyze.jpg" alt="Default Image 3">
+        <img class="hover-image" src="squaremodele.jpg" alt="Hover Image 3">
+        <p><b>COOKIE IN GLOSS BLACK</b></p>
+        <p><em>69,95 €</em></p>
+      </div>
+      <div class="image-container">
+        <img src="loewesyze.jpg" alt="Default Image 2">
+        <img class="hover-image" src="loewemodele.jpg" alt="Hover Image 2">
+        <p><b>Loewe Inflated Square sunglasses</b></p>
+        <p><em>244.88 €</em></p>
+      </div>
+       
+      <div class="image-container">
+        <img src="flamesyze.jpg" alt="Default Image 3">
+        <img class="hover-image" src="flamemodele.jpg" alt="Hover Image 3">
+        <p><b>Flame sunglasses</b></p>
+        <p><em>280.60 €</em></p>
+      </div>
+    
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+
+  <div class = "products">
+    <p><b>OUR NEWEST CAMPAIGN</b></p>
+        <div id="kontenti">
+          <header>
+          <img id="slideshow" />
+          </header>
+          <button onclick="changeImg()">Next</button>
+      </div>
+  
+      <script>
+          let i = 0;
+          let imgArray = ['campaign.jpg','campaign1.jpg','campaign2.jpg','campaign3.jpg','campaign4.jpeg'];
+  
+          function changeImg(){
+              document.getElementById('slideshow').src = imgArray[i];
+  
+              if(i< imgArray.length -1){
+                  i++;
+              }
+              else{
+                  i=0;
+              }
+              //setTimeout("changeImg()", 2600);
+          }
+          document.addEventListener(onload, changeImg());
+      </script>
+
+      </div>
+  </div>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<footer>
+  <div class="f">
+      <h2>About ClearVue Optics</h2>
+      <h2>Our Links</h2>
+      <div class="ff">
+          <a href=""><img src="instagram.png" alt="" style height="50px", width="50px"></a>
+          <a href=""><img src="twitter.png" alt="" style height="70px", width="70px"></a>
+          <a href=""><img src="facebook.png" alt="" style height="50px", width="50px"></a>
+          <a href=""><img src="pinterest.png" alt="" style height="50px", width="50px"></a>
+      </div>
+  </div>
+  <div class="footermain">
+      <div class="footerleft">
+          <p>From designer frames to cutting-edge lenses, we're your destination for fashionable eyewear and precise vision solutions.</p>
+      </div>
+      <div class="footercenter">
+          <p>Advertise</p>
+          <p>Support</p>
+          <p>Our Company</p>
+          <p>Contact</p>
+      </div>
+      <div class="footerright">
+          <p>Terms of use</p>
+          <p>Privacy Policy</p>
+      </div>
+  </div>
+  <div class="fundi">
+      <p>Copyright 2020 ClearVue Optics. All rights reserved.</p>
+  </div>
+</footer>
+<br>
+<br>
+<br>
+
+
+<body>
+    
+</body>
+</html>
